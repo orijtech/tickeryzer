6c854b2c96a994743644a71f2049a5040b381f4a
Cuong Manh Le <cuong@orijtech.com>
2021-01-04 12:55:05 +0700

    More test cases

--- testdata/src/a/a.go
+++ testdata/src/a/a.go
@@ -17,3 +17,20 @@ func g(ticker *time.Ticker) { // BAD: this should be reported
 		fmt.Println("Current time: ", t)
 	}
 }
+
+func h() {
+	ticker1 := time.NewTicker(time.Second) // want `missing ticker1.Stop\(\) call`
+	defer Stop()
+	ticker2 := time.NewTicker(time.Second) // want `missing ticker2.Stop\(\) call`
+	defer Stop()
+	go func() {
+		for range ticker2.C {
+		}
+	}()
+
+	for t := range ticker1.C {
+		fmt.Println("Current time: ", t)
+	}
+}
+
+func Stop() {}
